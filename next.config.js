fc242f2911f554035093b327628ef287d5dd6d35
Meerch <khukhriansk333@gmail.com>
2022-06-01 17:35:16 +0300

    change config nextjs

--- next.config.js
+++ next.config.js
@@ -4,9 +4,7 @@ const nextConfig = {
     images: {
         loader: "akamai",
         path: ''
-    },
-    basePath: "/goblin-ass",
-    assetPrefix: "/goblin-ass",
+    }
 }
 
 module.exports = nextConfig
