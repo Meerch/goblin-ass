c642dac39596dee76f2c33b32defaf71d860d4fe
Meerch <56489774+Meerch@users.noreply.github.com>
2022-06-01 14:04:30 +0000

    Deploying to gh-pages from @ Meerch/goblin-ass@8ac37761a16faeb837d2ae4f1cf1cdfbd06219a4 🚀

---
+++ _next/static/zwDX0D-DfkRc-mAeqYemS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/227-29f1069579e778bc.js","static/chunks/pages/index-fa14fc687d9ce1c7.js"],"/_error":["static/chunks/pages/_error-d419484d69bbcf53.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
